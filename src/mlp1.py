c7e009576f1e7cae30209a6cd94d2a6b23f24bd1
philipobi <philipobiu@gmail.com>
2025-03-21 17:55:17 +0100

    Implement classes for grid projection feature

--- src/mlp1.py
+++ src/mlp1.py
@@ -203,36 +203,78 @@ class Layer:
         self.W = np.random.rand(i, j)
         self.b = np.random.rand(j)
 
-class ProjectionLayer:
-    def __init__(self, layer, W=None, b=None):
+
+class ProjectionAxis:
+    def __init__(self, arr, pos, num, d):
+        self.arr = arr
+        self.pos = pos
+        self.num = num
+        self.d = d
+        self.redraw_ax(all=True)
+
+    @property
+    def in_range(self):
+        x0 = self.arr[..., *self.pos]
+        return x0 >= self.xmin and x0 <= self.xmax
+
+    def redraw_ax(self, all=False):
+        if all:
+            self.arr_ax = np.repeat(self.arr[np.newaxis], axis=0, repeats=self.num)
+        x0 = self.arr[..., *self.pos]
+        self.xmin = x0 - self.d
+        self.xmax = x0 + self.d
+        self.arr_ax[:, ..., *self.pos] = np.linspace(self.xmin, self.xmax, num=self.num)
+
+class LayerWrapper:
+    def __init__(self, layer):
         self.layer = layer
+
+class ProjectionLayer(LayerWrapper):
+    def __init__(self, layer, W=None, b=None):
+        super().__init__(layer)
+        
         self.redraw_hooks = []
+        self.ax_W = W
+        self.ax_b = b
 
         if W is None:
-            self.W_ = lambda: self.layer.W
+            self.W_ = np.copy(self.layer.W)
+            self.redraw_hooks.append(lambda: np.copyto(src=self.layer.W, dst=self.W_))
+            self.W_getter = lambda: self.W_
         else:
-            pass
+            self.redraw_hooks.append(lambda: W.redraw_ax(all=True))
+            self.W_getter = lambda: W.arr_ax
+
         if b is None:
-            self.b_ = lambda: self.layer.b
+            self.b_ = np.copy(self.layer.b)
+            self.redraw_hooks.append(lambda: np.copyto(src=self.layer.b, dst=self.b_))
+            self.b_getter = lambda: self.b_
         else:
-            pass
+            self.redraw_hooks.append(lambda: b.redraw_ax(all=True))
+            self.b_getter = lambda: b.arr_ax
 
-    @property
-    def W(self):
-        return self.W_()
-
-    @property
-    def b(self):
-        return self.b_()
-
-    def redraw(self):
+    def redraw_all(self):
         for fn in self.redraw_hooks:
             fn()
 
+    @property
+    def W(self):
+        return self.W_getter()
 
-class GridProjection:
-    def __init__(self, layers):
+    @property
+    def b(self):
+        return self.b_getter()
+
+
+class ProjectionGrid:
+    def __init__(self, layers, X, Y):
+        self.X = X
+        self.Y = Y
         self.model = mlogit()
+
+        self.axes = []
+        self.axes.extend([ax for layer in layers if (ax := layer.ax_W) is not None])
+        self.axes.extend([ax for layer in layers if (ax := layer.ax_b) is not None])
 
         self.layers = layers[:-1]
         self.layer_out = layers[-1]
@@ -243,8 +285,12 @@ class GridProjection:
 
         self.layer_out.ff = ff()
 
-    def run(self, X, Y):
-        A = X
+    def compute(self):
+        for ax in self.axes:
+            if not ax.in_range:
+                ax.redraw()
+
+        A = self.X
         for layer in self.layers:
             layer.ff.run(A, layer.W, layer.b)
             layer.act.run(ff.Z)
@@ -252,8 +298,16 @@ class GridProjection:
 
         layer = self.layer_out
         layer.ff.run(A, layer.W, layer.b)
-        self.model.run(layer.Z, Y, A, bprop=False)
+        self.model.run(layer.Z, self.Y, A, bprop=False)
 
+    @property
+    def grid(self):
+        return self.model.loss
+
+class Training:
+    def __init__(self, layers):
+        self.layers = [LayerWrapper(layer) for layer in layers]
+        
 
 def main():
     # init
